be0b571949ef75f58cfae96032a4b6e26d08a765
wi1dcode <magosdk@icloud.com>
2022-08-04 10:55:22 +0200

    step 4 done

--- src/pages/Popular.js
+++ src/pages/Popular.js
@@ -43,18 +43,3 @@ const Popular = () => {
 }
 
 export default Popular
-
-// adult: false
-// backdrop_path: "/p1F51Lvj3sMopG948F5HsBbl43C.jpg"
-// genre_ids: (3) [28, 12, 14]
-// id: 616037
-// original_language: "en"
-// original_title: "Thor: Love and Thunder"
-// overview: "After his retirement is interrupted by Gorr the God Butcher, a galactic killer who seeks the extinction of the gods, Thor enlists the help of King Valkyrie, Korg, and ex-girlfriend Jane Foster, who now inexplicably wields Mjolnir as the Mighty Thor. Together they embark upon a harrowing cosmic adventure to uncover the mystery of the God Butcher’s vengeance and stop him before it’s too late."
-// popularity: 13162.608
-// poster_path: "/pIkRyD18kl4FhoCNQuWxWu5cBLM.jpg"
-// release_date: "2022-07-06"
-// title: "Thor: Love and Thunder"
-// video: false
-// vote_average: 6.7
-// vote_count: 1554
